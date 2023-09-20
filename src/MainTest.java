ad3b845dc4e4ceceafee7f2048c3339544257995
Nguyễn Đăng Dương <21020757@vnu.edu.vn>
2023-09-21 03:07:55 +0700

    fix

--- src/MainTest.java
+++ src/MainTest.java
@@ -4,41 +4,43 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
+    /*
+    Subject:
+    1 - Normal households
+    2 - Near-poor households
+    3 - Poor households
+     */
     @org.junit.jupiter.api.Test
     void discount() {
         /*
         Normal households test
          */
         assertEquals(-1, Main.discount(-1, 1));
-        assertEquals(-1, Main.discount(100, 1));
+        assertEquals(-1, Main.discount(1000, 1));
         assertEquals(0, Main.discount(0, 1));
-        assertEquals(0, Main.discount(2, 1));
-        assertEquals(0, Main.discount(4, 1));
-        assertEquals(10, Main.discount(5, 1));
-        assertEquals(15, Main.discount(5, 1));
-        assertEquals(0, Main.discount(20, 1));
+        assertEquals(0, Main.discount(1, 1));
+        assertEquals(0, Main.discount(3, 1));
+        assertEquals(0, Main.discount(10, 1));
 
 
         /*
         Near-poor households test
          */
         assertEquals(-1, Main.discount(-1, 2));
-        assertEquals(-1, Main.discount(100, 2));
+        assertEquals(-1, Main.discount(1000, 2));
         assertEquals(0, Main.discount(0, 2));
         assertEquals(0, Main.discount(2, 2));
-        assertEquals(10, Main.discount(4, 2));
-        assertEquals(15, Main.discount(20, 2));
-        assertEquals(10, Main.discount(4, 2));
-        assertEquals(20, Main.discount(40, 2));
+        assertEquals(10, Main.discount(3, 2));
+        assertEquals(15, Main.discount(10, 2));
 
         /*
         Poor households test
          */
         assertEquals(-1, Main.discount(-1, 3));
-        assertEquals(-1, Main.discount(100, 3));
+        assertEquals(-1, Main.discount(1000, 3));
         assertEquals(0, Main.discount(0, 3));
         assertEquals(10, Main.discount(2, 3));
         assertEquals(15, Main.discount(4, 3));
-        assertEquals(20, Main.discount(20, 3));
+        assertEquals(20, Main.discount(10, 3));
     }
 }
